704358af5b7b43545ffc6450aad952a31ae7f8f5
xyz6510 <55530255+xyz6510@users.noreply.github.com>
2020-12-20 15:44:05 +0100

    Update lib.c

--- lib.c
+++ lib.c
@@ -15,8 +15,8 @@ int clock_gettime(clockid_t clockid,struct timespec *ts)
 
     int ret=clock_gettime_o(clockid,ts);
     if (clockid==CLOCK_MONOTONIC_RAW) {
-    if (sec_old==0) sec_old=ts->tv_sec;
-    ts->tv_sec-=sec_old;
+        if (sec_old==0) sec_old=ts->tv_sec;
+        ts->tv_sec-=sec_old;
     }
 
     return ret;
